914d2a7101bcc533435117fb46c3d4a4dc0a43e7
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-18 01:04:33 +0100

    Add script for copying file into dist

---
+++ scripts/copy-to-dist.cjs
@@ -0,0 +1,12 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const inputPath = process.argv.at(-1);
+const cwd = process.cwd();
+
+const source = path.join(cwd, inputPath);
+const destination = path.join(cwd, 'dist', 'ngx-testing-extra');
+
+Promise.all([fs.access(source), fs.access(destination)])
+  .then(() => fs.cp(source, path.join(destination, path.basename(source))))
+  .then(() => console.info(`${source} copied to ${destination}.`))
