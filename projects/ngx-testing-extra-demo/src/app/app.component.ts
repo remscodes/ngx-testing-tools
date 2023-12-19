758e31a90e83454152086907c34e42027fed9ca8
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-20 00:36:59 +0100

    Remove generated example's codes

--- projects/ngx-testing-extra-demo/src/app/app.component.ts
+++ projects/ngx-testing-extra-demo/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngx-testing-extra-demo';
+
 }
