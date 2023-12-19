2fdafa210a5815d68d663ea58c443f3e8ab59ee8
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-19 22:09:36 +0100

    Add guard test utils

---
+++ projects/ngx-testing-extra/src/lib/guard/activate.ts
@@ -0,0 +1,12 @@
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { buildRouteSnapshot, RouteSnapshotConfig } from '../route/route-snapshot';
+import { DefaultGuardReturn, GuardReturn } from './models/guard.model';
+
+export function challengeActivate<R extends GuardReturn = DefaultGuardReturn>(
+  guard: CanActivateFn,
+  state: RouterStateSnapshot,
+  routeConfig?: RouteSnapshotConfig,
+): R {
+  const route: ActivatedRouteSnapshot = buildRouteSnapshot(routeConfig);
+  return guard(route, state) as R;
+}
---
+++ projects/ngx-testing-extra/src/lib/guard/models/guard.model.ts
@@ -0,0 +1,10 @@
+import { UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export type GuardReturn =
+  | Observable<boolean | UrlTree>
+  | Promise<boolean | UrlTree>
+  | boolean
+  | UrlTree;
+
+export type DefaultGuardReturn = boolean;
