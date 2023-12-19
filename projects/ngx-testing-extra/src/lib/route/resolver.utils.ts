45b730ad5b81de7b92e6cccc3e6ec81f304ad641
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-20 00:20:36 +0100

    Add resolver testing

---
+++ projects/ngx-testing-extra/src/lib/route/resolver.utils.ts
@@ -0,0 +1,11 @@
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { DefaultResolverReturn, ResolveReturn } from './models/resolver.models';
+import { buildRouteSnapshot, RouteSnapshotConfig } from './route-snapshot';
+
+export function checkResolver<T, R extends ResolveReturn<T> = DefaultResolverReturn<T>>(resolver: ResolveFn<T>,
+  state: RouterStateSnapshot,
+  routeConfig?: RouteSnapshotConfig,
+): R {
+  const route: ActivatedRouteSnapshot = buildRouteSnapshot(routeConfig);
+  return resolver(route, state) as R;
+}
