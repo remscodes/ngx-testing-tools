805d658dc0bd93df14b315dfe2f527c29a685fa6
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-12 00:00:30 +0100

    Refactor

--- projects/ngx-testing-extra/src/lib/components/test-bed/assert-component.ts
+++ projects/ngx-testing-extra/src/lib/components/test-bed/assert-component.ts
@@ -2,7 +2,7 @@ import { Component, Type } from '@angular/core';
 import { Nullable } from '../../models/shared.model';
 import { isComponentAnnotation } from './component-annotation';
 
-export function assertComponent(TypeCtor: Type<any>, annotation: Nullable<Component>) {
+export function assertComponent(TypeCtor: Type<any>, annotation: Nullable<Component>): void {
   if (!isComponentAnnotation(annotation))
     throw new Error(`The provided "${TypeCtor.name}" is not a Component. Cannot create a ComponentTestBed.`);
 }
--- projects/ngx-testing-extra/src/lib/components/test-bed/component-annotation.ts
+++ projects/ngx-testing-extra/src/lib/components/test-bed/component-annotation.ts
@@ -1,4 +1,4 @@
-import { Component, Directive, NgModule, Pipe, Type } from '@angular/core';
+import { Component, Type } from '@angular/core';
 import { Nullable } from '../../models/shared.model';
 
 export function isComponentAnnotation(annotation: Nullable<Component>): boolean {
@@ -6,13 +6,11 @@ export function isComponentAnnotation(annotation: Nullable<Component>): boolean 
     || (annotation?.template !== undefined);
 }
 
-// Inspired by https://github.com/angular/angular/blob/1f8c53cd0c6c54b9d017f888567b85683ab0d348/packages/core/testing/src/resolvers.ts#L48
 export function getComponentAnnotation(ComponentCtor: Type<any>): Nullable<Component> {
   const annotations = (ComponentCtor as any)['__annotations__'];
   for (let i = annotations.length - 1; i >= 0; i --) {
     const annotation = annotations[i];
-    const isKnownType = (annotation instanceof Directive) || (annotation instanceof Component) || (annotation instanceof Pipe) || (annotation instanceof NgModule);
-    if (isKnownType) return (annotation instanceof Component) ? annotation : null;
+    if (annotation instanceof Component) return annotation;
   }
   return null;
 }
