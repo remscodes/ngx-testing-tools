cde258ff0a32636364ef144876d295fdf3941b76
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-12 01:41:51 +0100

    Add tests

--- projects/ngx-testing-extra/src/tests/units/components/test-bed/assert-component.spec.ts
+++ projects/ngx-testing-extra/src/tests/units/components/test-bed/assert-component.spec.ts
@@ -3,24 +3,30 @@ import { assertComponent } from '../../../../lib/components/test-bed/assert-comp
 import { getFirstAnnotation } from '../../../fixtures/helpers/annotations/get-first-annotation';
 
 describe('assertComponent', () => {
+  @Component({ template: `` })
+  class AppComponent {}
+
+  @Directive({ selector: '' })
+  class AppDirective {}
 
   it('should pass', () => {
-    @Component({ template: `` })
-    class AppComponent {}
-
     const annotation = getFirstAnnotation<Component>(AppComponent);
 
     expect(() => assertComponent(AppComponent, annotation))
       .not.toThrowError();
   });
 
-  it('should throw error', () => {
-    @Directive({ selector: '' })
-    class AppDirective {}
-
+  it('should throw error with AppDirective indication', () => {
     const annotation = getFirstAnnotation<Directive>(AppDirective);
 
     expect(() => assertComponent(AppDirective, annotation))
       .toThrowError('The provided "AppDirective" is not a Component. Cannot create a ComponentTestBed.');
+  });
+
+  it('should throw error with [object Object] indication', () => {
+    const annotation = getFirstAnnotation<Directive>(AppDirective);
+
+    expect(() => assertComponent({} as any, annotation))
+      .toThrowError('The provided "[object Object]" is not a Component. Cannot create a ComponentTestBed.');
   });
 });
---
+++ projects/ngx-testing-extra/src/tests/units/components/test-bed/component-annotation.spec.ts
@@ -0,0 +1,43 @@
+import { Component, Directive } from '@angular/core';
+import { getComponentAnnotation, isComponentAnnotation } from '../../../../lib/components/test-bed/component-annotation';
+
+describe('Component Annotation', () => {
+
+  describe('isComponentAnnotation', () => {
+
+    it('should be a component annotation', () => {
+      expect(isComponentAnnotation({ templateUrl: '' })).toBeTrue();
+    });
+
+    it('should be a component annotation', () => {
+      expect(isComponentAnnotation({ template: '' })).toBeTrue();
+    });
+
+    it('should not be a component annotation', () => {
+      expect(isComponentAnnotation({})).toBeFalse();
+    });
+  });
+
+  describe('getComponentAnnotation', () => {
+
+    it('should get component annotation', () => {
+      @Component({ template: '' })
+      class AComponent {}
+
+      expect(getComponentAnnotation(AComponent)).toBeTruthy();
+    });
+
+    it('should not get component annnotation', () => {
+      class NotAComponent {}
+
+      expect(getComponentAnnotation(NotAComponent)).toBeNull();
+    });
+
+    it('should not get component annnotation', () => {
+      @Directive({ selector: '[a]' })
+      class ADirective {}
+
+      expect(getComponentAnnotation(ADirective)).toBeNull();
+    });
+  });
+});
