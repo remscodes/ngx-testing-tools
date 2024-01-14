faebe2a86d08777b9bfd1bea0eca1ca9fc649234
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-14 14:55:28 +0100

    Update resolver type

--- projects/ngx-testing-extra/src/lib/router/resolver/resolver.ts
+++ projects/ngx-testing-extra/src/lib/router/resolver/resolver.ts
@@ -1,8 +1,10 @@
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
-import { DefaultResolverReturn, ResolveReturn } from '../models/resolver.models';
+import { Observable } from 'rxjs';
+import { ResolveReturn } from '../models/resolver.models';
 import { buildRouteSnapshot, RouteSnapshotConfig } from '../route-snapshot';
 
-export function checkResolver<T, R extends ResolveReturn<T> = DefaultResolverReturn<T>>(resolver: ResolveFn<T>,
+export function checkResolver<T, R extends ResolveReturn<T> = Observable<T>>(
+  resolver: ResolveFn<T>,
   state: RouterStateSnapshot,
   routeConfig?: RouteSnapshotConfig,
 ): R {
