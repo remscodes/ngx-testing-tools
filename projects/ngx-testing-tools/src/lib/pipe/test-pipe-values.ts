223c1efc8ed1551a8e8ed7b0f7a23b69eccdf2f2
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-18 21:18:58 +0100

    Add pipe tests

--- projects/ngx-testing-tools/src/lib/pipe/test-pipe-values.ts
+++ projects/ngx-testing-tools/src/lib/pipe/test-pipe-values.ts
@@ -1,7 +1,6 @@
 import { PipeTransform } from '@angular/core';
 
 export function testPipeValues<T extends PipeTransform>(pipe: T, record: Record<any, string>): void {
-
   Object.entries(record).forEach(([current, expected]: [any, string]) => {
     it(`should transform "${current}" to "${expected}"`, () => {
       expect(pipe.transform(current)).toEqual(expected);
