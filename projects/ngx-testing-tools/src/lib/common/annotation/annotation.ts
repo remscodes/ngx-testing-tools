2d6f4059b6f93b0485f122a226f5bf88a33b743b
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-20 15:51:47 +0100

    Get annotations

---
+++ projects/ngx-testing-tools/src/lib/common/annotation/annotation.ts
@@ -0,0 +1,24 @@
+import { Type } from '@angular/core';
+import { Nullable } from '../../models/shared.model';
+
+type MetadataName =
+  | 'Component'
+  | 'Directive'
+  | 'Injectable'
+  | 'NgModule'
+  | 'Pipe'
+
+export function getAnnotation<T, A>(AnyCtor: Type<T>, metadataName: MetadataName): Nullable<A> {
+  const annotations = (AnyCtor as any)['__annotations__'];
+  if (!annotations) return null;
+
+  for (let i = annotations.length - 1; i >= 0; i --) {
+    const annotation = annotations[i];
+    if (isSameMetadataName(annotation, metadataName)) return annotation;
+  }
+  return null;
+}
+
+function isSameMetadataName(annotation: any, metadataName: MetadataName): boolean {
+  return Object.getPrototypeOf(annotation)['ngMetadataName'] === metadataName;
+}
---
+++ projects/ngx-testing-tools/src/lib/common/annotation/ng-module-annotation.ts
@@ -0,0 +1,7 @@
+import { NgModule, Type } from '@angular/core';
+import { Nullable } from '../../models/shared.model';
+import { getAnnotation } from './annotation';
+
+export function getNgModuleAnnotation<T>(NgModuleCtor: Type<T>): Nullable<NgModule> {
+  return getAnnotation(NgModuleCtor, 'NgModule');
+}
