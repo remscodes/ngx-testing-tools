df0f71f40de41d1fa824c810656884f6ced0b99d
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-26 12:16:38 +0100

    Deprecate the usage of query and action utilities outside `ComponentTestBed`

--- projects/ngx-testing-tools/src/lib/component/query/find-all-debug-elements.ts
+++ projects/ngx-testing-tools/src/lib/component/query/find-all-debug-elements.ts
@@ -3,8 +3,11 @@ import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { throwCannotFind } from './cannot-find';
 
+/** @deprecated Use `ComponentTestBed` instead to access this utility. Will be removed in v3. */
 export function findAllDebugElements(fixture: ComponentFixture<any>, selector: string): DebugElement[]
+/** @deprecated Use `ComponentTestBed` instead to access this utility. Will be removed in v3. */
 export function findAllDebugElements(fixture: ComponentFixture<any>, directive: Type<any>): DebugElement[]
+/** @deprecated Use `ComponentTestBed` instead to access this utility. Will be removed in v3. */
 export function findAllDebugElements(fixture: ComponentFixture<any>, selectorOrDirective: string | Type<any>): DebugElement[]
 export function findAllDebugElements(fixture: ComponentFixture<any>, selectorOrDirective: string | Type<any>): DebugElement[] {
   const debugs: DebugElement[] = (typeof selectorOrDirective === 'string')
