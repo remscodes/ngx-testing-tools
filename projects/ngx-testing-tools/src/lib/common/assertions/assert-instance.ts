76629a9e33c745ef75411153345f69bd341da777
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-05-10 02:14:37 +0200

    Fix type

--- projects/ngx-testing-tools/src/lib/common/assertions/assert-instance.ts
+++ projects/ngx-testing-tools/src/lib/common/assertions/assert-instance.ts
@@ -1,6 +1,8 @@
 import { Type } from '@angular/core';
 import { throwInstanceError } from '../errors/throw-instance-error';
 
-export function assertInstance(instance: unknown, Ctor: Type<unknown>): void {
+export function assertInstance(instance: unknown, Ctor: Type<unknown> | AbstractType<unknown>): void {
   if (!(instance instanceof Ctor)) throwInstanceError({ name: Ctor.name });
 }
+
+type AbstractType<T = any> = abstract new (...args: any[]) => T;
